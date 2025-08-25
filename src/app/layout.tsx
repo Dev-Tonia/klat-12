82a560d98d895b94a18a2f434800b08201753d42
Dev-Tonia <ogunwaanthonia@gmail.com>
2025-08-25 16:16:59 +0100

    feat implemented the routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1 className=" text-red-500">nav bar</h1>
         {children}
       </body>
     </html>
