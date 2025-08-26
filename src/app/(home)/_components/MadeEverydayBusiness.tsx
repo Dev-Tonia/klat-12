5da5b8bcf2a2377d117ff7845271ea6573a82409
Dev-Tonia <ogunwaanthonia@gmail.com>
2025-08-26 17:20:00 +0100

    feat: implemented the hero section

---
+++ src/app/(home)/_components/MadeEverydayBusiness.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MadeEverydayBusiness() {
+  return (
+    <section className=" bg-white px-[clamp(1rem,5vw,112px)] pt-15.5 ">
+      <div className=" tracking-tighter text-primary max-w-xl mx-auto flex flex-col gap-5 text-center">
+        <h2 className=" font-goodly font-semibold text-6xl leading-16">
+          Made for Everyday and Business Use
+        </h2>
+        <p className=" font-dm-sans text-xl px-9 ">
+          Whether you’re sending a quick transfer or running a business, KLAT-32
+          makes it easy to get paid
+        </p>
+      </div>
+    </section>
+  );
+}
